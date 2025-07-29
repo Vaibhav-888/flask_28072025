8e118650e685e8a1fe992ece6e593f280ccb5b8c
Lohar <vaibhav-ratilal.lohar@capgemini.com>
2025-07-29 23:55:12 +0530

    2nd commit

--- app.py
+++ app.py
@@ -15,21 +15,24 @@ stores = [
 ]
 
 
-@app.get("/") # http://127.0.0.1:8080/
+@app.get("/") # endpoint: http://127.0.0.1:8080/
 def get_stores():
     return {"stores": stores}
 
 
-@app.post("/store")  # http://127.0.0.1:8080/store
+@app.post("/store")  # endpoint: http://127.0.0.1:8080/store
 def create_store():
+    """Create stores"""
     request_data = request.get_json()
     new_store = {"name": request_data["name"], "items": []}
     stores.append(new_store)
     return new_store, 201 # 201 resource created
 
 
-@app.post("/store/<string:name>/item")
+
+@app.post("/store/<string:name>/item") # endpoint: http://127.0.0.1:8080/My%Store/item
 def create_item(name):
+    """Create item in a store"""
     request_data = request.get_json()
     for store in stores:
         if store["name"] == name:
@@ -39,5 +42,31 @@ def create_item(name):
     return {"messege": "Store not found"}, 404 # 404 Store not found
 
 
-if __name__ == '__main__':
+# get stores and items that initially created.
+
+@app.get("/store")
+def get_store_data():
+    """get a store data"""
+    for store in stores:
+        return {"store": store}
+        
+@app.get("/store/<string:name>") # endpoint: http://127.0.0.1:8080/My%Store
+def get_store(name):
+    """Get a Store"""
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"messege": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>/item")
+def get_items_in_store(name):
+    """Get items from a particular store"""
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"messege": "items not found"}, 404
+
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port= 8080, debug=True)
